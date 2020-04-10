9896e84c53f72cf3c5bc4726b86c8403a587e1b9
Tom O'Donnell <dev.todonnell@gmail.com>
2020-04-10 13:04:47 -0400

    Added Love Letter
    Added Love Letter card game to codex.

---
+++ codex/loveletter.py
@@ -0,0 +1,13 @@
+loveletter = {
+    "name": "loveletter",
+    "cards": [
+        { "name": "Princess", "count": 1, "description": "lose if discarded" },
+        { "name": "Countess", "count": 1, "description": "must be played if you have a king or princess in your hand" },
+        { "name": "King", "count": 1, "description": "trade hands with another player" },
+        { "name": "Prince", "count": 2, "description": "choose a player; they discard their hand and draw a new card" },
+        { "name": "Handmaid", "count": 2, "description": "you cannot be chosen until your next turn" },
+        { "name": "Baron", "count": 2, "description": "compare hands with another player; lower number is out" },
+        { "name": "Priest", "count": 2, "description": "look at a player's hand" },
+        { "name": "Guard", "count": 5, "description": "guess a player's hand; if correct, player is out" },
+    ],
+}
